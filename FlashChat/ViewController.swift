b9e4a583c054a9fd71c31d11ecd99ee2a8ce9582
Amondus <50753491+Amondus@users.noreply.github.com>
2019-12-18 12:57:41 +0300

    Initial Commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Антон Захарченко on 18.12.2019.
+//  Copyright © 2019 Антон Захарченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
